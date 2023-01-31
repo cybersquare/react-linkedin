4db5c1ed6f08fdaf5afe2709e3a449bd9363fad3
Muhammad Shahid Rafi C P - MacBook Pro <muhammadshahidraficp@gmail.com>
2023-01-31 21:50:19 +0530

    post ui completed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -6,8 +6,19 @@ import Subscriptions from '@mui/icons-material/Subscriptions';
 import InputOption from "../InputOption/InputOption";
 import "./Feed.css";
 import { Avatar } from '@mui/material';
+import Post from "../Post/Post"
 
 const Feed = () => {
+
+    let posts = [
+        {name:"Shahid", description:"inting and typesetting", message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",photoUrl:""},
+        {name:"Malik Sha", description:"Lorem Ipsum is simply dummy text of the", message:"",photoUrl:""},
+        {name:"Vishnu Gopal", description:"printing and typesetting industry.", message:"",photoUrl:""},
+        {name:"Ravi Pillai", description:" Lorem Ipsum has been the industry's", message:"",photoUrl:""},
+        {name:"Menon ", description:" Lorem Ipsum has been the industry's", message:"",photoUrl:""},
+        {name:"Devaraj", description:"inting and typesetting ", message:"",photoUrl:""}
+    ];
+
     return(
         <div className="feed">
             <div className="feed_inputContainer">
@@ -25,6 +36,8 @@ const Feed = () => {
                     <InputOption title="Write Article" Icon={CalendarViewDay} color="#e16744"/>
                 </div>
             </div>
+            {
+            posts.map((post,index) => <Post description={post.description} name={post.name} message={post.message} />)}
         </div>
     );
 }
---
+++ src/components/Post/Post.js
@@ -0,0 +1,32 @@
+import InputOption from "../InputOption/InputOption";
+import ThumbUpAltOutlined from "@mui/icons-material/ThumbUpAltOutlined";
+import ChatOutlined from "@mui/icons-material/ChatOutlined";
+import ShareOutlined from "@mui/icons-material/ShareOutlined";
+import SendOutlined from "@mui/icons-material/SendOutlined";
+
+import "./Post.css";
+import { Avatar } from "@mui/material";
+
+
+function Post({name, description, message,photoUrl}){
+    return(<div className="post">
+        <div className="post__header">
+            <Avatar/>
+            <div className="post__info">
+                <h2>{name}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+        <div className="post__body">
+            <p>{message}</p>
+        </div>
+        <div className="post__buttons">
+            <InputOption Icon={ThumbUpAltOutlined} title="Like"/>
+            <InputOption Icon={ChatOutlined} title="Comment"/>
+            <InputOption Icon={ShareOutlined} title="Share"/>
+            <InputOption Icon={SendOutlined} title="Send"/>
+        </div>
+    </div>);
+} 
+
+export default Post;
